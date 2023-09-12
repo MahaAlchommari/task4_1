d3377eb0e193ded3be3f923a5968703aef515137
MahaAlchommari <mahaalchommari@gmail.com>
2023-09-12 15:08:20 +0300

    task4-1

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 /////////////////////////part one////////////////////////////////////
-var num = [2, 4, 6, 8, 10, 12, 11, 20, 21, 23];
 
+var numbers = [];
 for (var i = 0; i < 10; i++) {
-  num.push(Math.floor(Math.random() * 10));
+  numbers.push(Math.floor(Math.random() * 10));
 }
 
-var sumEven = 0;
-for (var i = 0; i < num.length; i++) {
-  if (num[i] % 2 === 0) {
-    sumEven += num[i];
+var evenSum = 0;
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenSum += numbers[i];
+  }
+}
+var sumLess = 0;
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 5) {
+    sumLess += numbers[i];
   }
 }
 
-console.log("adding even numbers: " + sumEven);
-
-var sumLessThan5 = 0;
-for (var i = 0; i < num.length; i++) {
-  if (num[i] < 5) {
-    sumLessThan5 += num[i];
-  }
-}
-
-console.log("the sum of numbers whose is less than 5: " + sumLessThan5);
+console.log("Random numbers: " + numbers.join(", "));
+console.log("adding even numbers:" + evenSum);
+console.log("the sum of numbers whose is less than 5: " + sumLess);
 
 /////////////////////////////////////////////////////////////
 
