7e5d0a73cda422ec720a8d71d2a917d645f5a589
MahaAlchommari <mahaalchommari@gmail.com>
2023-09-12 21:17:47 +0300

    task4-1

--- main.js
+++ main.js
@@ -21,18 +21,31 @@ for (var i = 0; i < numbers.length; i++) {
 console.log("Random numbers: " + numbers.join(", "));
 console.log("adding even numbers:" + evenSum);
 console.log("the sum of numbers whose is less than 5: " + sumLess);
+document.write("Random numbers: " + numbers.join(", ") + "<br>");
+document.write("adding even numbers:" + evenSum + "<br>");
+document.write("the sum of numbers whose is less than 5: " + sumLess + "<br>");
 
 /////////////////////////////////////////////////////////////
 
+/////////////////////////part 2////////////////////////////////////
 var user = {
   name: "Maha",
   number: 123456789,
   age: 23,
   email: "maha@example.com",
 };
+
 console.log(user);
 
 console.log("name: " + user.name);
 console.log("num:" + user.number);
 console.log("age: " + user.age);
-console.log("email " + user.email);
+console.log("email: " + user.email);
+
+document.write(JSON.stringify(user) + "<br>");
+document.write(user + "<br>");
+document.write("name: " + user.name + "<br>");
+document.write("num:" + user.number + "<br>");
+document.write("age " + user.age + "<br>");
+document.write("email: " + user.email + "<br>");
+/////////////////////////////////////////////////////////////
